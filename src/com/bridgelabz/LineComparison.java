1435dfb0c20755b90bd60daf8b1cc28ab756c3cd
priya <priyapandey8884.blr@gmail.com>
2022-08-05 01:17:48 +0530

    ADDED:[Priya] Displayed Welcome Message for LineComparison Problem

---
+++ src/com/bridgelabz/LineComparison.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * 
+ * @author pande
+ *
+ */
+
+public class LineComparison {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to Line Comparision Computation");
+	
+		  
+
+}
+}
